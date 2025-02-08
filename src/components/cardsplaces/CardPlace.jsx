bff0ff2de6b7070b66903cfc18ba38be98305161
EckiHag <eu@hagemeier-web.de>
2025-02-08 17:14:41 +0100

    Initial commit

--- src/components/cardsplaces/CardPlace.jsx
+++ src/components/cardsplaces/CardPlace.jsx
@@ -16,23 +16,19 @@ export default function CardPlace({ id, image, title, description }) {
           </CardBody>
         </ScrollShadow>
         <CardFooter className="flex flex-row gap-2 items-center justify-start mt-auto">
-          <Link href={`/pics/cards/${id}`}>
+          {/* <Link href={`/pics/cards/${id}`}>
             <Button as="div" size="sm" className="bg-blue-400">
               PicCards
             </Button>
-          </Link>
-          <Link href={`/pics/gallery/${id}`}>
-            <Button as="div" size="sm" className="bg-blue-400">
-              PicGallery
-            </Button>
-          </Link>
-          <Link href={`/pics/work/${id}`}>
-            <Button as="div" size="sm" className="bg-blue-400">
-              Works
-            </Button>
-          </Link>
+          </Link> */}
+          <Button showAnchorIcon as={Link} color="primary" href={`/pics/cards/${id}`} variant="solid">
+            PicCards
+          </Button>
+          <Button showAnchorIcon as={Link} color="primary" href={`/pics/gallery/${id}`} variant="solid">
+            PicGallery
+          </Button>
           <Button showAnchorIcon as={Link} color="primary" href={`/pics/work/${id}`} variant="solid">
-            Button Link
+            Works
           </Button>
         </CardFooter>
       </Card>

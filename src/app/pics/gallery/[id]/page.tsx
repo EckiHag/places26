bce23318fc4bcc33ff8d2464a2ad782afbf6a3ba
EckiHag <eu@hagemeier-web.de>
2025-02-14 10:45:53 +0100

    Initial commit

--- src/app/pics/gallery/[id]/page.tsx
+++ src/app/pics/gallery/[id]/page.tsx
@@ -1,44 +1,54 @@
 "use server";
 
-import { getPicsByBelongstoid } from "@/app/actions/picActions";
-// import { getPicsTwenty } from "@/app/actions/picActions";
-import GalleryPage from "./GalleryPage";
+import React from "react";
 
-// interface Params {
-//   params: {
-//     id: string;
-//   };
-// }
+function page() {
+  return <div>Zum Testen</div>;
+}
+
+export default page;
+
+// "use server";
+
+// import { getPicsByBelongstoid } from "@/app/actions/picActions";
+// // import { getPicsTwenty } from "@/app/actions/picActions";
+// import GalleryPage from "./GalleryPage";
+
+// // interface Params {
+// //   params: {
+// //     id: string;
+// //   };
+// // }
+
+// // export default async function PlacesWithCreatorsubject({ params }: { params: { id: string } }) {
+// //   const { id } = params;
 
 // export default async function PlacesWithCreatorsubject({ params }: { params: { id: string } }) {
 //   const { id } = params;
 
-export default async function PlacesWithCreatorsubject({ params }: { params: { id: string } }) {
-  const { id } = params;
+//   if (!id) {
+//     throw new Error("Die ID in den Params fehlt.");
+//   }
 
-  if (!id) {
-    throw new Error("Die ID in den Params fehlt.");
-  }
+//   const pics = (await getPicsByBelongstoid(id)) ?? [];
+//   const sortedPics = pics && pics.sort((a, b) => new Date(b.created).getTime() - new Date(a.created).getTime());
 
-  const pics = (await getPicsByBelongstoid(id)) ?? [];
-  const sortedPics = pics && pics.sort((a, b) => new Date(b.created).getTime() - new Date(a.created).getTime());
+//   // const sls = await getPicsTwenty();
+//   // console.log("sls: ", sls);
 
-  // const sls = await getPicsTwenty();
-  // console.log("sls: ", sls);
+//   const slides = sortedPics?.map((slide) => ({
+//     ...slide, // Behalte alle ursprünglichen Eigenschaften
+//     src: `https://beihaggis.de/${slide.image?.replace(/^.\//, "")}`, // Transformiere `image` zu `src`
+//     image: undefined, // Entferne die ursprüngliche `image`-Eigenschaft
+//     imgwidth: slide.imgwidth ?? undefined, // `null` in `undefined` umwandeln
+//     imgheight: slide.imgheight ?? undefined, // `null` in `undefined` umwandeln
+//     title: slide.title ?? undefined, // `null` in `undefined` umwandeln
+//     description: slide.description ?? undefined, // `null` in `undefined` umwandeln
+//     copyright: slide.copyright ?? undefined, // `null` in `undefined` umwandeln
+//   }));
 
-  const slides = sortedPics?.map((slide) => ({
-    ...slide, // Behalte alle ursprünglichen Eigenschaften
-    src: `https://beihaggis.de/${slide.image?.replace(/^.\//, "")}`, // Transformiere `image` zu `src`
-    image: undefined, // Entferne die ursprüngliche `image`-Eigenschaft
-    imgwidth: slide.imgwidth ?? undefined, // `null` in `undefined` umwandeln
-    imgheight: slide.imgheight ?? undefined, // `null` in `undefined` umwandeln
-    title: slide.title ?? undefined, // `null` in `undefined` umwandeln
-    description: slide.description ?? undefined, // `null` in `undefined` umwandeln
-    copyright: slide.copyright ?? undefined, // `null` in `undefined` umwandeln
-  }));
+//   console.log("Gallery pics (slides): ", slides);
+//   console.log("Gallery pics (slides): Ende");
 
-  console.log("Gallery pics (slides): ", slides);
-  console.log("Gallery pics (slides): Ende");
-
-  return <GalleryPage slides={slides ?? []} />;
-}
+//   return <GalleryPage slides={slides ?? []} />;
+// }

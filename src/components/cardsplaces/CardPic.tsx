f9142ff9d81f41d53540ebfe30f1d7e98fe0b048
EckiHag <eu@hagemeier-web.de>
2025-02-20 17:03:59 +0100

    Initial commit

--- src/components/cardsplaces/CardPic.tsx
+++ src/components/cardsplaces/CardPic.tsx
@@ -1,26 +1,26 @@
 "use client";
 import { Card, CardHeader, CardBody, Divider, Image } from "@heroui/react";
 
-interface PicProps {
-  pic?: {
-    title?: string;
-    description?: string;
-    image?: string;
-  };
+interface CardPicProps {
+  id: string;
+  image: string | null;
+  title: string;
+  description: string;
 }
 
-export default function CardPic({ pic }: PicProps) {
+export default function CardPic({ image, title, description }: CardPicProps) {
+  // console.log("Id von pic für die Card: ", id);
   return (
     <Card className="max-w-[400px] min-h-[200px]">
       <CardHeader className="flex gap-3 bg-ppics-400">
         <div className="flex flex-col">
-          <p className="text-2xl">{pic?.title}</p>
+          <p className="text-2xl">{title}</p>
         </div>
       </CardHeader>
       <Divider />
       <CardBody className="bg-ppics-100 items-center">
-        {pic?.image && <Image isZoomed alt="NextUI place Image" src={`https://beihaggis.de/${pic.image.replace(/^.\//, "")}`} width={300} />}
-        <span className="text-xs">{pic?.description}</span>
+        {image && <Image isZoomed alt="NextUI place Image" src={`https://beihaggis.de/${image.replace(/^.\//, "")}`} width={300} />}
+        <span className="text-xs">{description}</span>
       </CardBody>
     </Card>
   );

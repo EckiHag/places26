5fe90a016c012d3c18810807d9029a8d37fbe367
EckiHag <eu@hagemeier-web.de>
2025-02-20 18:58:28 +0100

    Initial commit

--- src/app/pics/page.tsx
+++ src/app/pics/page.tsx
@@ -1,11 +1,11 @@
 "use server";
 
 import { getPicsTwenty } from "@/app/actions/picActions";
-import CardPic from "@/components/cardsplaces/CardPic";
+// import CardPic from "@/components/cardsplaces/CardPic";
 
 export default async function PicsList() {
   const pics = await getPicsTwenty();
-  const sortedPics = pics && pics.sort((a, b) => new Date(b.created).getTime() - new Date(a.created).getTime());
+  // const sortedPics = pics && pics.sort((a, b) => new Date(b.created).getTime() - new Date(a.created).getTime());
 
   console.log("Fetched Pics:", JSON.stringify(pics, null, 2));
 
@@ -15,12 +15,12 @@ export default async function PicsList() {
       <br></br>
       <div className="flex justify-center">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 max-w-6xl mx-auto justify-items-center">
-          {sortedPics &&
+          {/* {sortedPics &&
             sortedPics.map((pic) => (
               <div key={pic.id}>
                 <CardPic pic={pic} />
               </div>
-            ))}
+            ))} */}
         </div>
       </div>
     </div>

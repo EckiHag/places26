ca8a1b5c470c6b70f7188f372cd731ddb862847d
EckiHag <eu@hagemeier-web.de>
2025-02-25 21:04:13 +0100

    Initial commit

--- src/app/pics/cards/[id]/page.tsx
+++ src/app/pics/cards/[id]/page.tsx
@@ -4,24 +4,20 @@ import Link from "next/link";
 import CardPic from "@/components/cardsplaces/CardPic";
 
 interface PageProps {
-  params: Promise<{ id: string }>;
+  params: { id: string };
   searchParams: { subjectId?: string };
 }
 
 export default async function PicsCardWithPlaceId({ params, searchParams }: PageProps) {
-  const resolvedParams = await params;
-  const { id } = resolvedParams;
-  // const { id } = params;
+  const { id } = params;
   const { subjectId } = searchParams;
   const pics = await getPicsByBelongstoid(id);
   const place = await getPlaceById(id);
   const sortedPics = pics && pics.sort((a, b) => new Date(b.created).getTime() - new Date(a.created).getTime());
-  // const sortedPics = pics?.sort((a, b) => new Date(b.created.$date) - new Date(a.created.$date));
-  // console.log("pics: ", pics);
+
   return (
     <>
       <div className="mb-6 text-center">
-        {/* Hier muss eine deleteAction ausgeführt werden */}
         <Link href={`/pics/editpic/new?placeId=${id}`}>New Pic</Link>
       </div>
       <div className="mt- mb-2">
@@ -29,7 +25,6 @@ export default async function PicsCardWithPlaceId({ params, searchParams }: Page
           Back to Places
         </Link>
       </div>
-
       <div className="flex justify-center">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 max-w-6xl mx-auto justify-items-center">
           {sortedPics &&

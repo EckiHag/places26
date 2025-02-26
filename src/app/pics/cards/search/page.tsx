7c8a325c05790b85207815eb7ea362985e835845
EckiHag <eu@hagemeier-web.de>
2025-02-26 16:21:59 +0100

    Initial commit

--- src/app/pics/cards/search/page.tsx
+++ src/app/pics/cards/search/page.tsx
@@ -3,9 +3,12 @@ import { getPlaceById } from "@/app/actions/placeActions";
 import Link from "next/link";
 import CardPic from "@/components/cardsplaces/CardPic";
 
-export default async function PicsCardWithPlaceId({ searchParams }: { searchParams: Record<string, string | undefined> }) {
-  const id = searchParams?.id;
-  const subjectId = searchParams?.subjectId;
+export default async function PicsCardWithPlaceId({ searchParams }: { searchParams?: { [key: string]: string | string[] | undefined } }) {
+  const id = typeof searchParams?.id === "string" ? searchParams.id : undefined;
+  const subjectId = typeof searchParams?.subjectId === "string" ? searchParams.subjectId : undefined;
+
+  console.log("PicsCardWithPlaceId subjectId:", subjectId);
+  console.log("PicsCardWithPlaceId id:", id);
 
   console.log("PicsCardWithPlaceId subjectId:", subjectId);
   console.log("PicsCardWithPlaceId id:", id);

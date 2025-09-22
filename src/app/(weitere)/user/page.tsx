8fe98f205cc84d3958c7f555723109cb3208e4a8
EckiHag <eu@hagemeier-web.de>
2025-09-22 08:37:25 +0200

    Refactored EditPicPage to use HeroUI components

--- src/app/(weitere)/user/page.tsx
+++ src/app/(weitere)/user/page.tsx
@@ -5,7 +5,7 @@ import UsersTableClient from "./userTableClient"; // Client-Komponente separat
 
 export default async function UserTabelle() {
   const users: User[] = (await getUserAll()) ?? [];
-
+  console.log("users: ", users);
   return (
     <div className="p-6">
       <h2 className="text-3xl font-bold text-gray-800 mb-6">User-Verwaltung</h2>
